e49e3f9c96ba836e571365ee145d74e86596511d
Alexander <3517142+sitesmake@users.noreply.github.com>
2025-02-17 22:23:28 +0300

    Auth Helper

--- ImageFeed/Auth/WebViewPresenter.swift
+++ ImageFeed/Auth/WebViewPresenter.swift
@@ -21,25 +21,14 @@ final class WebViewPresenter: WebViewPresenterProtocol {
         static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
     }
 
+    var authHelper: AuthHelperProtocol
+
+    init(authHelper: AuthHelperProtocol) {
+        self.authHelper = authHelper
+    }
+
     func viewDidLoad() {
-        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else {
-            print("Set urlComponents error")
-            return
-        }
-
-        urlComponents.queryItems = [
-            URLQueryItem(name: "client_id", value: Constants.accessKey),
-            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
-            URLQueryItem(name: "response_type", value: "code"),
-            URLQueryItem(name: "scope", value: Constants.accessScope)
-        ]
-
-        guard let url = urlComponents.url else {
-            print("Error set url")
-            return
-        }
-
-        let request = URLRequest(url: url)
+        guard let request = authHelper.authRequest() else { return }
 
         didUpdateProgressValue(0)
 
@@ -59,14 +48,6 @@ final class WebViewPresenter: WebViewPresenterProtocol {
     }
 
     func code(from url: URL) -> String? {
-        if let urlComponents = URLComponents(string: url.absoluteString),
-           urlComponents.path == "/oauth/authorize/native",
-           let items = urlComponents.queryItems,
-           let codeItem = items.first(where: { $0.name == "code" })
-        {
-            return codeItem.value
-        } else {
-            return nil
-        }
+        authHelper.code(from: url)
     }
 }
