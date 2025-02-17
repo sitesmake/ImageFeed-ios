0c119b9c161147b9e4bb947725460f0851d970d6
Alexander <3517142+sitesmake@users.noreply.github.com>
2025-02-17 22:16:39 +0300

    Add config

---
+++ ImageFeed/AuthConfiguration.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+enum Constants {
+    static let accessKey = "DxzdeHBjA3_csQMSL1tsTIgDikpOwdel4MUKOkILNvE"
+    static let secretKey = "puecuqcFRIBDl5t8NfA9w0_4wG-4goAeKWb1GMsRRRM"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+}
+
+struct AuthConfiguration {
+    let accessKey: String
+    let secretKey: String
+    let redirectURI: String
+    let accessScope: String
+    let defaultBaseURL: URL
+    let authURLString: String
+
+    init(accessKey: String, secretKey: String, redirectURI: String, accessScope: String, authURLString: String, defaultBaseURL: URL) {
+        self.accessKey = accessKey
+        self.secretKey = secretKey
+        self.redirectURI = redirectURI
+        self.accessScope = accessScope
+        self.defaultBaseURL = defaultBaseURL
+        self.authURLString = authURLString
+    }
+
+    static var standard: AuthConfiguration {
+        return AuthConfiguration(accessKey: Constants.accessKey,
+                                 secretKey: Constants.secretKey,
+                                 redirectURI: Constants.redirectURI,
+                                 accessScope: Constants.accessScope,
+                                 authURLString: Constants.unsplashAuthorizeURLString,
+                                 defaultBaseURL: Constants.defaultBaseURL)
+    }
+}
